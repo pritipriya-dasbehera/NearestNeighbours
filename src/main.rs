128dc7fed7357e60d90321e7090f5ece6960ca7d
Pritipriya <pritiprya.dasbehera@gmail.com>
2022-07-25 12:02:09 +0530

    Array3 grid method working

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use std::fs::File;
 use std::time::{self, Duration};
 use rand::Rng;
 // use arr_macro::arr;
-// use ndarray::{Array3};
+use ndarray::{Array3};
 
-const NUM_OF_PARTICLES: usize = 100;
+const NUM_OF_PARTICLES: usize = 100000;
 const NEAREST_NEIGHBOURS_REQ: usize = 10;
 const MAX_DIST: f64 = 0.8660254037844387;
-// const GRID_SIZE: usize = 6;
-// const GRID_LEN: f64 = 1.0/6.0;
+const GRID_SIZE: usize = 45;
+const GRID_LEN: f64 = 1.0/45.0;
 // const MAX_DIST: f64 = 1.0;
 
 fn main() {
@@ -34,26 +34,30 @@ fn main() {
     println!("The time taken to initialise {} particle nearlist is {} microseconds.",NUM_OF_PARTICLES,t_init.as_micros());
     // print_nearlist(&nearlist);
 
-    // now = time::Instant::now();
-    // let mut grid = Array::from_elem((GRID_SIZE,GRID_SIZE,GRID_SIZE), vec![0_usize]);
-    // create_grid(&points, &mut grid);
-    // let t_grid = now.elapsed();
-    // println!("The time taken to make grid of {} particles is {} microseconds.",NUM_OF_PARTICLES,t_grid.as_micros());
+    now = time::Instant::now();
+    let mut grid = Array3::from_elem((GRID_SIZE,GRID_SIZE,GRID_SIZE), vec![0_usize]);
+    create_grid(&points, &mut grid);
+    let t_grid = now.elapsed();
+    println!("The time taken to make grid of {} particles is {} microseconds.",NUM_OF_PARTICLES,t_grid.as_micros());
 
-    // write_grid(&grid);
+    write_grid(&grid);
 
     now = time::Instant::now();
-    brute_cal_nearest(&points, &mut nearlist);
-    let t_brute = now.elapsed();
-    println!("The time taken to brute calc {} particle nearlist is {} microseconds.",NUM_OF_PARTICLES,t_brute.as_micros());
+    grid_calc_nearest(&points, &grid, &mut nearlist);
+    let t_gridcalc = now.elapsed();
+    println!("The time taken to grid calc {} particle nearlist is {} microseconds.",NUM_OF_PARTICLES,t_gridcalc.as_micros());
+
+    // now = time::Instant::now();
+    // brute_cal_nearest(&points, &mut nearlist);
+    // let t_brute = now.elapsed();
+    // println!("The time taken to brute calc {} particle nearlist is {} microseconds.",NUM_OF_PARTICLES,t_brute.as_micros());
 
     now = time::Instant::now();
     write_nearlist(&nearlist);
     let elapsed_time = now.elapsed();
     println!("The time taken to write {} particle nearlist is {} microseconds.",NUM_OF_PARTICLES,elapsed_time.as_micros());
-    // print_nearlist(&nearlist);
 
-    write_time(t_point_create, t_point_write, t_init, t_brute);
+    // write_time(t_point_create, t_point_write, t_init, t_grid+t_gridcalc);
 }
 
 // #[derive(Debug)]
@@ -107,10 +111,20 @@ fn point_generator(points: &mut Vec<Point>){
 
 }
 
-// fn print_nearlist(nearlist:&[[Node;NEAREST_NEIGHBOURS_REQ+1];NUM_OF_PARTICLES]){
-//     for (i,element) in nearlist.iter().enumerate(){
-//         println!("The array {i} is {:?}",element)
+// fn print_nearlist(nearlist:&Vec<Node>){
+//     for i in 0..NUM_OF_PARTICLES //*(NEAREST_NEIGHBOURS_REQ+1)
+//     {
+//         let mut data = String::new();
+//         for (j,element) in nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)..i*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].iter().enumerate(){
+//             if j==0{data.push_str(&i.to_string());continue};
+//             if j==NEAREST_NEIGHBOURS_REQ+1{break;};
+//             data.push(',');
+//             data.push_str(&element.id.to_string());
+//         }
+//         data.push('\n');
+//         print!("{data}");
 //     }
+//     print!("\n");
 // }
 
 fn write_points(points: &Vec<Point>){
@@ -169,29 +183,30 @@ fn write_time(t_point_create:Duration,t_point_write:Duration,t_init:Duration,t_b
 
 }
 
-// fn create_grid(points: &[Point], grid:&mut Array3<Vec<usize>>){
-//     for (i,point) in points.iter().enumerate(){
-//         let x = unsafe { (point.x/GRID_LEN).floor().to_int_unchecked()};
-//         let y = unsafe { (point.y/GRID_LEN).floor().to_int_unchecked()};
-//         let z = unsafe { (point.z/GRID_LEN).floor().to_int_unchecked()};
-//         grid[[x,y,z]][0] += 1;
-//         let index = grid[[x,y,z]][0];
-//         grid[[x,y,z]].insert(index, i);
-//     }
-// }
+fn create_grid(points: &[Point], grid:&mut Array3<Vec<usize>>){
+    for (i,point) in points.iter().enumerate(){
+        let x = (point.x/GRID_LEN) as usize;
+        let y = (point.y/GRID_LEN) as usize;
+        let z = (point.z/GRID_LEN) as usize;
+        
+        grid[[x,y,z]][0] += 1;
+        let index = grid[[x,y,z]][0];
+        grid[[x,y,z]].insert(index, i);
+    }
+}
 
-// fn write_grid(grid:&Array3<Vec<usize>>){
-//     let mut file = File::create("grid.csv").expect("Something went wrong in file creation");
-//     for cell in grid.iter(){
-//         let mut data = String::new();
-//         for id in cell.iter(){
-//             data.push_str(&id.to_string());
-//             data.push(',')
-//         }
-//         data.push('\n');
-//         file.write_all(data.as_bytes()).expect("Couldnt write the points");
-//     }
-// }
+fn write_grid(grid:&Array3<Vec<usize>>){
+    let mut file = File::create("grid.csv").expect("Something went wrong in file creation");
+    for cell in grid.iter(){
+        let mut data = String::new();
+        for id in cell.iter(){
+            data.push_str(&id.to_string());
+            data.push(',')
+        }
+        data.push('\n');
+        file.write_all(data.as_bytes()).expect("Couldnt write the points");
+    }
+}
 
 // fn write_grid(grid:&Array3<Vec<usize>>){
 //     let mut file = File::create("nearlist.csv").expect("Something went wrong in file creation");
@@ -203,44 +218,107 @@ fn write_time(t_point_create:Duration,t_point_write:Duration,t_init:Duration,t_b
 //             )))
 // }
 
-fn brute_cal_nearest(points: &Vec<Point>, nearlist:&mut Vec<Node>){
-    // let mut file = File::create("nearlist.csv").expect("Something went wrong in file creation");
-    // let mut file = File::options().append(true).open("nearlist.csv").expect("Something went wrong in file creation");
-    // {0..NUM_OF_PARTICLES}.for_each(|i|
-    for i in 0..NUM_OF_PARTICLES
-    {
-        for j in 0..NUM_OF_PARTICLES
-        {
-            if i == j
-            {
-                continue;
-            }
-            let d = dist(&points[i],&points[j]);
-            // print!("The distance between point {i} and point {j} is {d}\n");
-            if d<nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)].dist{
-                // print!("Inserting node for {i}\t");
-                let mut k = 1;
-                while k<NEAREST_NEIGHBOURS_REQ && nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k].dist < d{
-                    k+=1;
-                }
-                nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k..=i*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].rotate_right(1);
-                nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k] = Node{id:j as u32,dist:d};
-                nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)].dist = nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].dist;
-                // print_nearlist(nearlist);
-                // println!("\n");
+// fn brute_cal_nearest(points: &Vec<Point>, nearlist:&mut Vec<Node>){
+//     // let mut file = File::create("nearlist.csv").expect("Something went wrong in file creation");
+//     // let mut file = File::options().append(true).open("nearlist.csv").expect("Something went wrong in file creation");
+//     // {0..NUM_OF_PARTICLES}.for_each(|i|
+//     for i in 0..NUM_OF_PARTICLES
+//     {
+//         for j in 0..NUM_OF_PARTICLES
+//         {
+//             if i == j
+//             {
+//                 continue;
+//             }
+//             let d = dist(&points[i],&points[j]);
+//             // print!("The distance between point {i} and point {j} is {d}\n");
+//             if d<nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)].dist{
+//                 // print!("Inserting node for {i}\t");
+//                 let mut k = 1;
+//                 while k<NEAREST_NEIGHBOURS_REQ && nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k].dist < d{
+//                     k+=1;
+//                 }
+//                 nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k..=i*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].rotate_right(1);
+//                 nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+k] = Node{id:j as u32,dist:d};
+//                 nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)].dist = nearlist[i*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].dist;
+//                 // print_nearlist(nearlist);
+//                 // println!("\n");
+//             }
+//         }
+//         // let mut data = String::new();
+//         // for (j,element) in nearlist[i].iter().enumerate(){
+//         //     if j==0{data.push_str(&i.to_string());continue};
+//         //     if j==NEAREST_NEIGHBOURS_REQ+1{break;};
+//         //     data.push_str(",");
+//         //     data.push_str(&element.id.to_string());
+//         // }
+//         // data.push_str("\n");
+//         // file.write_all(data.as_bytes()).expect("unable to write");
+//     }
+// }
 
-            }
-        }
-        // let mut data = String::new();
-        // for (j,element) in nearlist[i].iter().enumerate(){
-        //     if j==0{data.push_str(&i.to_string());continue};
-        //     if j==NEAREST_NEIGHBOURS_REQ+1{break;};
-        //     data.push_str(",");
-        //     data.push_str(&element.id.to_string());
-        // }
-        // data.push_str("\n");
-        // file.write_all(data.as_bytes()).expect("unable to write");
-    }
+fn grid_calc_nearest(points: &Vec<Point>,grid:&Array3<Vec<usize>>, nearlist:&mut Vec<Node>, ){
+    let mut buffer: i64 = 1;
+    (0..GRID_SIZE).for_each(|i|{
+        (0..GRID_SIZE).for_each(|j|{
+            (0..GRID_SIZE).for_each(|k|{
+                let mut size:usize = grid[[i,j,k]].len();
+                loop{
+                    if size>=NEAREST_NEIGHBOURS_REQ{break;}
+                    size = 0;
+                    (i as i64-buffer..=i as i64+buffer).for_each(|mut ti|
+                        (j as i64-buffer..=j as i64+buffer).for_each(|mut tj|
+                            (k as i64-buffer..=k as i64+buffer).for_each(|mut tk|{
+                                if ti < 0 {ti = GRID_SIZE as i64+ti} else if ti >= GRID_SIZE as i64 {ti = ti - GRID_SIZE as i64};
+                                if tj < 0 {tj = GRID_SIZE as i64+tj} else if tj >= GRID_SIZE as i64 {tj = tj - GRID_SIZE as i64};
+                                if tk < 0 {tk = GRID_SIZE as i64+tk} else if tk >= GRID_SIZE as i64 {tk = tk - GRID_SIZE as i64};
+                                let ti = ti as usize;
+                                let tj = tj as usize;
+                                let tk = tk as usize;
+                                // println!("The size is {size}");
+                                size += grid[[ti,tj,tk]][1..].len();
+                                // println!("The size is {size}");
+                            })
+                        )
+                    );
+                    // println!("The size is finally {size}\n\n");
+                    if size>=NEAREST_NEIGHBOURS_REQ{break;}
+                    // println!("Inside loop");
+                    buffer+=1;
+                };
+                // println!("Grid {i},{j},{k} has buffer {buffer}");
+                grid[[i,j,k]][1..].iter().for_each(|point1_id|{
+                    let point1_id=point1_id.clone();
+                    // println!("Comparing from point{point1_id}");
+                    (i as i64-buffer..=i as i64+buffer).for_each(|mut ti|
+                        (j as i64-buffer..=j as i64+buffer).for_each(|mut tj|
+                            (k as i64-buffer..=k as i64+buffer).for_each(|mut tk|{
+                                if ti < 0 {ti = GRID_SIZE as i64+ti} else if ti >= GRID_SIZE as i64 {ti = ti - GRID_SIZE as i64};
+                                if tj < 0 {tj = GRID_SIZE as i64+tj} else if tj >= GRID_SIZE as i64 {tj = tj - GRID_SIZE as i64};
+                                if tk < 0 {tk = GRID_SIZE as i64+tk} else if tk >= GRID_SIZE as i64 {tk = tk - GRID_SIZE as i64};
+                                let ti = ti as usize;
+                                let tj = tj as usize;
+                                let tk = tk as usize;
+                                for point2_id in grid[[ti,tj,tk]][1..].iter(){
+                                    let point2_id=point2_id.clone();
+                                    // if n==0{ if point2_id==0{break;} else{continue;};};
+                                    if point1_id==point2_id{continue;}
+                                    // println!("In grid {i},{j},{k} point{point1_id} compared to point{point2_id} in grid {ti},{tj},{tk} with buffer{buffer}");
+                                    let d = dist(&points[point1_id],&points[point2_id]);
+                                    if d<nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)].dist{
+                                        // print!("Inserting node for {i}\t");
+                                        let mut k = 1;
+                                        while k<NEAREST_NEIGHBOURS_REQ && nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)+k].dist < d {k+=1;}
+                                        nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)+k..=point1_id*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].rotate_right(1);
+                                        nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)+k] = Node{id:point2_id as u32,dist:d};
+                                        nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)].dist = nearlist[point1_id*(NEAREST_NEIGHBOURS_REQ+1)+NEAREST_NEIGHBOURS_REQ].dist;
+                                    }
+                                }
+                            })
+                        )
+                    );
+                });
+            })
+        })
+    });
 }
-
-// fn grid_calc_nearest()
